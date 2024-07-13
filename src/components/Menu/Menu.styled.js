6438c8401c7cb25f8668e803805871358e5881c2
VuktorTS <skalina222@gmail.com>
2024-07-13 15:49:31 +0300

    add menuItems arr

---
+++ src/components/Menu/Menu.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const MenuList = styled.ul`
+  display: flex;
+  gap: 16px;
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  justify-content: flex-end;
+`;
